c138ddab3bd4be8345c9f8837c9c99b4793eb5ff
Pedro <pbruno1283@gmail.com>
2021-09-18 12:15:52 -0300

    first commit

---
+++ app/src/main/java/com/example/perguntas_e_respostas/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.perguntas_e_respostas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
