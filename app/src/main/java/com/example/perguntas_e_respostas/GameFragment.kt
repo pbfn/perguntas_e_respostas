bd6bdfcc802cf85d545f6e9553b7f3a6a0f38a2f
Pedro <pbruno1283@gmail.com>
2021-09-18 14:43:56 -0300

    Gerada a função generateQuestions()

--- app/src/main/java/com/example/perguntas_e_respostas/GameFragment.kt
+++ app/src/main/java/com/example/perguntas_e_respostas/GameFragment.kt
@@ -10,13 +10,14 @@ import androidx.navigation.fragment.navArgs
 import com.example.perguntas_e_respostas.databinding.FragmentGameBinding
 
 
-class GameFragment : Fragment(),View.OnClickListener {
+class GameFragment : Fragment(), View.OnClickListener {
 
-    private  var _binding: FragmentGameBinding?= null
+    private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
     private var qtd_perguntas: Int? = null
-
-
+    private var qtd_acertos: Int = 0
+    private val resp: Boolean? = null
+    private var questions = IntArray(6)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +33,7 @@ class GameFragment : Fragment(),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        generateQuestions()
         setClicksEvents()
         getArgs()
     }
@@ -44,31 +46,29 @@ class GameFragment : Fragment(),View.OnClickListener {
 
     override fun onClick(v: View?) {
 
-
-        when(v?.id){
-
+        when (v?.id) {
             //marcar a alteranativa em azul
-            R.id.radio_button_resp_1 ->{
+            R.id.radio_button_resp_1 -> {
                 binding.buttonResp.isEnabled = true
             }
-            R.id.radio_button_resp_2 ->{
+            R.id.radio_button_resp_2 -> {
                 binding.buttonResp.isEnabled = true
             }
-            R.id.radio_button_resp_3 ->{
+            R.id.radio_button_resp_3 -> {
                 binding.buttonResp.isEnabled = true
             }
-            R.id.radio_button_resp_4 ->{
+            R.id.radio_button_resp_4 -> {
                 binding.buttonResp.isEnabled = true
             }
 
-            R.id.button_resp ->{
-                if(binding.buttonResp.text.equals("Responder")){
+            R.id.button_resp -> {
+                if (binding.buttonResp.text.equals("Responder")) {
                     binding.buttonResp.text = "Próxima pergunta"
-                    //TODO VERIFICAR SE É A CERTA
+                    //TODO Verificar se é a resposta correta e alterar a variável resp para true
                     desativeRadio()
 
 
-                }else{
+                } else {
                     nextFragment()
                 }
             }
@@ -77,7 +77,7 @@ class GameFragment : Fragment(),View.OnClickListener {
     }
 
     //Eventos de Click
-    private fun setClicksEvents(){
+    private fun setClicksEvents() {
         binding.buttonResp?.setOnClickListener(this)
         binding.radioButtonResp1?.setOnClickListener(this)
         binding.radioButtonResp2?.setOnClickListener(this)
@@ -86,7 +86,7 @@ class GameFragment : Fragment(),View.OnClickListener {
     }
 
     //Desativando os radio buttons para evitar de alterar a resposta após responder
-    private fun desativeRadio(){
+    private fun desativeRadio() {
         binding.radioButtonResp1.isEnabled = false
         binding.radioButtonResp2.isEnabled = false
         binding.radioButtonResp3.isEnabled = false
@@ -94,26 +94,36 @@ class GameFragment : Fragment(),View.OnClickListener {
     }
 
     //Varificando para qual fragment deve ir
-    private fun nextFragment(){
-        if(qtd_perguntas!! < 10){
-            //verificar se acertou e passar no total de acertos +1
-            qtd_perguntas = qtd_perguntas!! +1
-            val direction = GameFragmentDirections.actionGameFragmentSelf(qtd_perguntas!!,0)
+    private fun nextFragment() {
+        if (qtd_perguntas!! < 10) {
+            if(resp == true){
+                qtd_acertos = qtd_acertos!! + 1
+            }
+            qtd_perguntas = qtd_perguntas!! + 1
+            val direction = GameFragmentDirections.actionGameFragmentSelf(qtd_perguntas!!, qtd_acertos!!)
             findNavController().navigate(direction)
 
-        }else{
-            val direction = GameFragmentDirections.actionGameFragmentToResultFragment(0)
+        } else {
+            val direction = GameFragmentDirections.actionGameFragmentToResultFragment(qtd_acertos!!)
             findNavController().navigate(direction)
         }
     }
 
     //Pegando argumentos da tela anterior
-    private fun getArgs(){
-        val args : GameFragmentArgs by navArgs()
+    private fun getArgs() {
+        val args: GameFragmentArgs by navArgs()
         qtd_perguntas = args.quantidadePerguntas
-        binding.textViewPergunta?.text = "Pergunta nº" + qtd_perguntas.toString()
+        binding.textViewPergunta?.text = "Pergunta nº" + qtd_perguntas.toString() +": qual a soma de "+questions[0].toString() + "  " +questions[1].toString()
     }
 
 
+    private fun generateQuestions(){
+        questions[0] = (1..400).random()
+        questions[1] = (1..400).random()
+        questions[2] = (1..400).random()
+        questions[3] = (1..400).random()
+        questions[4] = (1..400).random()
+        questions[5] = questions[0] + questions[1]
+    }
 
 }
