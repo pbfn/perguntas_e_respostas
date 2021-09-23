31bf96442f80a21ba2ed03c4f9b68f50791fc84f
Pedro <pbruno1283@gmail.com>
2021-09-23 13:53:29 -0300

    Style dos checkbox alterados e funcionando

--- app/src/main/java/com/example/perguntas_e_respostas/GameFragment.kt
+++ app/src/main/java/com/example/perguntas_e_respostas/GameFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.RadioButton
 import androidx.core.content.ContextCompat
+import androidx.core.view.setPadding
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.perguntas_e_respostas.databinding.FragmentGameBinding
